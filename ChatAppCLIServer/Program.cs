e7751a49504d7f1b88550c298e3abaab5c4a04e6
Tomos Jones <tomoswiliamjones@gmail.com>
2024-12-01 23:53:46 +0000

    Add project files.

---
+++ ChatAppCLIServer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var webSocketServer = new WebSocketServer("ws://localhost:8080");
+            webSocketServer.AddWebSocketService<ChatBehaviour>("/Chat");
+            webSocketServer.Start();
+            Console.WriteLine($"Chat App Server Started at  {webSocketServer.Port}");
+            Console.WriteLine("Press Any Key Sto Stop The Server!");
+
+            Console.ReadKey();
+            webSocketServer.Stop();
+        }
+    }
+
+
+    public class ChatBehaviour : WebSocketBehavior
+    {
+       
+    }
+}
