05baa346a6e10ba811f07895b6063e92f477f5bd
Tomos Jones <tomoswiliamjones@gmail.com>
2024-12-04 02:30:38 +0000

    Wow, Done a Lot, Server is now working for broadcasting messages to clients!!!!

--- ChatAppCLIServer/Program.cs
+++ ChatAppCLIServer/Program.cs
@@ -1,27 +1,72 @@
 ﻿using System;
+using System.Diagnostics;
 using WebSocketSharp;
 using WebSocketSharp.Server;
+using ErrorEventArgs = WebSocketSharp.ErrorEventArgs;
 
 namespace ChatServer
 {
     class Program
     {
-        static void Main(string[] args)
+
+
+         static void Main(string[] args)
         {
-            var webSocketServer = new WebSocketServer("ws://localhost:8080");
-            webSocketServer.AddWebSocketService<ChatBehaviour>("/Chat");
+             int port;
+
+
+            Console.WriteLine("Enter Port Number To Start The Server");
+            string portString = Console.ReadLine();
+            
+            if(int.TryParse(portString, out port) == false)
+            {
+                Console.WriteLine("Invalid Port Number, Please Try Again!");
+                return;
+            }
+
+            var webSocketServer = new WebSocketServer($"ws://127.0.0.1:{port}");
             webSocketServer.Start();
-            Console.WriteLine($"Chat App Server Started at  {webSocketServer.Port}");
-            Console.WriteLine("Press Any Key Sto Stop The Server!");
+            webSocketServer.AddWebSocketService<ChatBehaviour>("/chat");
+
+            //This is only hardcoded now because it's always going to be hosted on localhost
+            Console.WriteLine($"Chat App Server Started at  127.0.0.1:{webSocketServer.Port}");
+            
+
+            Console.WriteLine("Press Any Key To Stop The Server!");
 
             Console.ReadKey();
             webSocketServer.Stop();
+            Console.WriteLine("Server Successfully Stopped!");
+
         }
+
+
+
     }
 
 
     public class ChatBehaviour : WebSocketBehavior
     {
-       
+       protected override void OnMessage(MessageEventArgs e)
+        {
+            Sessions.Broadcast($"From: {ID}: " + e.Data);
+            Console.WriteLine($"Received Message: {e.Data}");
+        }
+
+        protected override void OnOpen()
+        {
+            Console.WriteLine($"Client Connected: {ID}");
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            Console.WriteLine($"Client Disconnected: {ID}");
+        }
+
+        protected override void OnError(ErrorEventArgs e)
+        {
+            Console.WriteLine($"Error: {e}");
+            
+        }
     }
 }
